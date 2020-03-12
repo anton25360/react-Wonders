44567f5e38021826b98d09a43d4c01a9ba08d27b
Anton Webb <anton25360@gmail.com>
2020-03-12 23:06:14 +0000

    rounded card corners

--- src/components/WonderCard.js
+++ src/components/WonderCard.js
@@ -5,8 +5,10 @@ export class WonderCard extends Component {
     render() {
         return (
             <div className='cardContainer'>
-                <img src={this.props.imgLink} alt='background'/>
-                <h1 className='cardTitle'>{this.props.name} in {this.props.location}</h1>
+                <img className='cardBackground' src={this.props.imgLink} alt='background'/>
+                <h1 className='cardName'>{this.props.name}</h1>
+                <h1 className='cardLocation'>{this.props.location}</h1>
+            
             </div>
         )
     }
