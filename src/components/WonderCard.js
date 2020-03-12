dc42e4d946b45f4be4d79e49745e69789343b85f
Anton Webb <anton25360@gmail.com>
2020-03-12 22:32:49 +0000

    added image to card

--- src/components/WonderCard.js
+++ src/components/WonderCard.js
@@ -5,6 +5,7 @@ export class WonderCard extends Component {
     render() {
         return (
             <div className='cardContainer'>
+                <img src={this.props.imgLink} alt='image' />
                 <h1>{this.props.name} in {this.props.dateBuilt}</h1>
             </div>
         )
