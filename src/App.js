96ea7013ad5d0e41aa7d9249a0cfbe3d3a4b876f
Anton Webb <anton25360@gmail.com>
2020-03-12 23:56:00 +0000

    added local image links

--- src/App.js
+++ src/App.js
@@ -2,18 +2,27 @@ import React from 'react';
 import './App.css';
 import WonderCard from './components/WonderCard'
 
+//background images
+import taj from './res/taj.jpg';
+import colosseum from './res/colosseum.jpg';
+import chichen from './res/chichen.jpg';
+import machu from './res/machu.jpg';
+import christ from './res/christ.jpg';
+import petra from './res/petra.jpg';
+import great from './res/great.jpg';
+
 function App() {
 
   return (
     <div className="App">
       <h1>Wonders:</h1>
-      <WonderCard name='Taj Mahal' location='India' imgLink='https://picsum.photos/300/200'/><br/>
-      <WonderCard name='Colosseum' location='Italy' imgLink='https://www.worldatlas.com/r/w728/upload/55/2c/54/shutterstock-433413835.jpg'/><br/>
-      <WonderCard name='Chichen Itza' location='Mexico' imgLink={images[0]}/><br/>
-      <WonderCard name='Machu Picchu' location='Peru' imgLink='https://picsum.photos/300/200'/><br/>
-      <WonderCard name='Christ the Redeemer' location='Brazil' imgLink='https://picsum.photos/300/200'/><br/>
-      <WonderCard name='Petra' location='Jordan' imgLink='https://picsum.photos/300/200'/><br/>
-      <WonderCard name='Great Wall of China' location='China' imgLink='https://picsum.photos/300/200'/>
+      <WonderCard name='Taj Mahal' location='India' imgLink={taj}/><br/>
+      <WonderCard name='Colosseum' location='Italy' imgLink={colosseum}/><br/>
+      <WonderCard name='Chichen Itza' location='Mexico' imgLink={chichen}/><br/>
+      <WonderCard name='Machu Picchu' location='Peru' imgLink={machu}/><br/>
+      <WonderCard name='Christ the Redeemer' location='Brazil' imgLink={christ}/><br/>
+      <WonderCard name='Petra' location='Jordan' imgLink={petra}/><br/>
+      <WonderCard name='Great Wall of China' location='China' imgLink={great}/>
     </div>
   );
 }
