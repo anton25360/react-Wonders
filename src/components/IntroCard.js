fd2d42b49a54db69acb1eac4ab1338c7f54b68e2
Anton Webb <anton25360@gmail.com>
2020-03-13 04:05:48 +0000

    added gradient title

--- src/components/IntroCard.js
+++ src/components/IntroCard.js
@@ -5,7 +5,7 @@ export class IntroCard extends Component {
     render() {
         return (
             <div className='introContainer'>
-                <h1 className='introName'>Welcome to Wonders.</h1>
+                <h1 className='introName'>Welcome to <span className='introGradient'>Wonders.</span></h1>
                 <p className='introInfo'><br/>Here you can find information on all 7 (new) wonders of the world. You can also install 'Wonders' as a native app by clicking on Share > Add to Home Screen.<br/><br/>Enjoy!</p>
             </div>
         )
