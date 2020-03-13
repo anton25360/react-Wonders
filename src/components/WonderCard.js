1d643e055b8911772b6a4773da40814dcb70bba0
Anton Webb <anton25360@gmail.com>
2020-03-13 00:08:54 +0000

    Added placeholder info

--- src/components/WonderCard.js
+++ src/components/WonderCard.js
@@ -8,7 +8,7 @@ export class WonderCard extends Component {
                 <img className='cardBackground' src={this.props.imgLink} alt='background'/>
                 <h2 className='cardLocation'>{this.props.location}</h2>
                 <h1 className='cardName'>{this.props.name}</h1>
-
+                <p className='cardInfo'>Although the Great Pyramid of Giza was not officially selected as one of the new seven wonders of the world, it was assigned an honorary title because of its undeniable importance. The Great Pyramid of Giza is the biggest and the oldest one among the three pyramids forming the Giza Pyramid Complex.</p>
             
             </div>
         )
