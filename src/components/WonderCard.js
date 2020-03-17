1b800a4027709d9b26e6e30515774a4c517c768e
Anton Webb <anton25360@gmail.com>
2020-03-17 22:51:38 +0000

    Removed comments

--- src/components/WonderCard.js
+++ src/components/WonderCard.js
@@ -48,8 +48,6 @@ export class WonderCard extends Component {
                 <p className='cardInfo'>{this.props.info}</p>
                 <button onClick={() => { markButton(this.props.num) }} className='markBtn' id={this.props.num}>Mark as visited</button>
                 <img onClick={() => { removeFromStorage(this.props.num) }} class='revertBtn' id={this.props.num + 'revert'} src={undo} alt=''/>
-                {/* <button src={undo}></button> */}
-                {/* <img src={undo} /> */}
             </div>
         )
     }
