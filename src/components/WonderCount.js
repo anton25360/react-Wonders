2133d4dd8c4219c18a027c867acd1fbf007548d0
Anton Webb <anton25360@gmail.com>
2020-03-17 21:19:47 +0000

    Gives % progress

--- src/components/WonderCount.js
+++ src/components/WonderCount.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react'
 import './WonderCount.css';
 
 function getVisitedCount() {
-    var num = Object.keys(localStorage).length
-    document.getElementById('countMsg').innerText = 'hahahah'
-    return num
+    var visitedCount = Object.keys(localStorage).length
+    // document.getElementById('countMsg').innerText = 'hahahah'
+    if (visitedCount == 7) {
+        return 'You have visited all 7 Wonders. Congratulations!'
+    } else if (visitedCount == 0) {
+        return 'You haven\'t visited any yet. Get going!'
+    } else if (visitedCount == 1) {
+        return ' You have visited ' + visitedCount + ' Wonder. Nice!'
+    } else {
+        let math = visitedCount / 7 * 100
+        return ' You have visited ' + visitedCount + ' Wonders. (x% done) ' + math
+    }
 }
 
 function setMessage(params) {
-    
+
 }
 
 export class WonderCount extends Component {
