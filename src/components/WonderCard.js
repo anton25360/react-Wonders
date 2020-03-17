4216d8a3fbbce0284d8dba9ec4e61a5ac1001070
Anton Webb <anton25360@gmail.com>
2020-03-17 07:55:03 +0000

    added markSeen function that displays number onClick

--- src/components/WonderCard.js
+++ src/components/WonderCard.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react'
 import './WonderCard.css';
 
+
+function markSeen(input) {
+    alert(input)
+}
+
 export class WonderCard extends Component {
+
+
+
     render() {
         return (
             <div className='cardContainer'>
                 <img className='cardBackground' src={this.props.imgLink} alt='background' />
                 <h2 className='cardLocation'>{this.props.location}</h2>
                 <h1 className='cardName'>{this.props.name}</h1>
-                <p className='cardInfo'>{this.props.info}</p>
-
+                <p className='cardInfo'>{this.props.info}</p>    
+                <p onClick={()=>{markSeen(this.props.num)}} className='cardMark'>Mark as seen.</p>
             </div>
         )
     }
