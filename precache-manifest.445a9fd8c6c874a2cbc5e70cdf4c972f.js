0f10d5ccea9ddbffeae1c76ab067e0312bdf773b
Anton Webb <anton25360@gmail.com>
2020-03-18 14:29:13 +0000

    Updates

---
+++ precache-manifest.445a9fd8c6c874a2cbc5e70cdf4c972f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a68a202f751a35aaf1741b98dd47f564",
+    "url": "/react-wonders/index.html"
+  },
+  {
+    "revision": "a4c0916273cbe788a71f",
+    "url": "/react-wonders/static/css/main.d8cf0fe7.chunk.css"
+  },
+  {
+    "revision": "5d70e215bafa363bdfd7",
+    "url": "/react-wonders/static/js/2.e674c527.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-wonders/static/js/2.e674c527.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4c0916273cbe788a71f",
+    "url": "/react-wonders/static/js/main.1026cb3d.chunk.js"
+  },
+  {
+    "revision": "62e2bc83718db45a1ad4",
+    "url": "/react-wonders/static/js/runtime-main.432df6cb.js"
+  },
+  {
+    "revision": "716425e518cdef1b0fb5e2b381cea262",
+    "url": "/react-wonders/static/media/chichen.716425e5.jpg"
+  },
+  {
+    "revision": "6fca6cec45d6d5ea94cac7701fdbe80c",
+    "url": "/react-wonders/static/media/christ.6fca6cec.jpg"
+  },
+  {
+    "revision": "6b329f5f5daf625669a542adc44d2839",
+    "url": "/react-wonders/static/media/colosseum.6b329f5f.jpg"
+  },
+  {
+    "revision": "6557f359a3d8059505696ace65d71c06",
+    "url": "/react-wonders/static/media/great.6557f359.jpg"
+  },
+  {
+    "revision": "5aa2d9d7bf8c50f6e18b6790be27e42b",
+    "url": "/react-wonders/static/media/machu.5aa2d9d7.jpg"
+  },
+  {
+    "revision": "ee2214d435fccb191b547e92325e26cd",
+    "url": "/react-wonders/static/media/petra.ee2214d4.jpg"
+  },
+  {
+    "revision": "d4b3b7cf4a724ae2e7e0b6cc1b5f2720",
+    "url": "/react-wonders/static/media/taj.d4b3b7cf.jpg"
+  },
+  {
+    "revision": "b55b35db3bf80b305390429783a03ba0",
+    "url": "/react-wonders/static/media/undo.b55b35db.svg"
+  }
+]);
